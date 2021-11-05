b5421e5c57e35baf995444e73948a9e05750a947
Demmi <gerasimchick@tut.by>
2021-11-06 01:11:23 +0200

    feat: add copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 
+const currentPath = path.resolve(__dirname, 'files');
+const newPath = path.resolve(__dirname, 'files-copy');
+
+
+function createDir() {
+    fs.stat(newPath, (err) => {
+        if (err && err.code === 'ENOENT') {
+            fs.mkdir(newPath, (err) => {})
+        } else {
+            fs.rm(newPath, {force: true}, (err, files) => {})
+        }
+    })
+}
+
+function copyDirectory() {
+    createDir()
+    fs.readdir(currentPath, (err, files) => {
+        files.forEach(file => {
+            fs.copyFile(path.resolve(currentPath, file), path.resolve(newPath, file), (err) => {})
+        })
+    })
+}
+
+copyDirectory()
